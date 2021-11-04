371cb9481eba398b81b830623dc5133b45d589ba
Taiwo <taiwooyekanmi@gmail.com>
2021-11-03 21:20:57 -0400

    add colors for pokemon type

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,42 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
+  },
+
+  type: {
+    normal: "#AAA67F",
+    fighting: "#C12239",
+    flying: "#A891EC",
+    poison: "#A43E9E",
+    ground: "#DEC16B",
+    rock: "#B69E31",
+    bug: "#A7B723",
+    ghost: "#70559B",
+    steel: "#B7B9D0",
+    fire: "#F57D31",
+    water: "#6493EB",
+    grass: "#74CB48",
+    electric: "#F9CF30",
+    psychic: "#FB5584",
+    ice: "#9AD6DF",
+    dragon: "#7037FF",
+    dark: "#75574C",
+    fairy: "#E69EAC",
+    unknown: "#AAA67F",
+    shadow: "#70559B",
   },
 };
